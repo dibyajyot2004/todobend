bed3f585231a7d02520d5316f2c7f3dc2175720e
dibyajyoti mishra <112049492+dibyajyot2004@users.noreply.github.com>
2024-07-20 22:10:20 +0530

    Update conn.js

--- conn/conn.js
+++ conn/conn.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const conn = async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://dibya1234:dibya824@cluster0.wxsv20m.mongodb.net/'
+      'mongodb+srv://dibya1234:<password>@cluster0.wxsv20m.mongodb.net/'
     );
     console.log('Connected');
   } catch (error) {
